8756ce58ed28d7be6aba97b524f8b0b805c6813d
zhangyuhan <hi_zhangyuhan@163.com>
2018-05-30 16:12:26 +0800

    🌟 初始化

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const http = require('http')
+const express = require('express')
+const app = express()
+
+const opn = require('opn')
+const address = require('address')
+
+function staticServer ({
+    port = 1234,
+    baseUrl = '/',
+    outputDir = 'dist'
+} = {}){
+    let url = `http://${address.ip()}:${port}${baseUrl}`
+    // 指定映射目录
+    app.use(baseUrl, express.static(outputDir))
+    // 创建服务端
+    http.createServer(app).listen(port, function () {
+        console.log(`本地测试服务器启动:  ${url}`)
+        opn(url)
+    });
+}
+module.exports = staticServer
